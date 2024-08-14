4187c0a4e0b5613de21b253aeeb3c3f29711a352
SaraSosman <sarasosman96@gmail.com>
2024-08-14 19:07:57 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,22 @@
+const app = Vue.createApp({
+    data() {
+      return {
+        goals: [],
+        enteredValue: '',
+      };
+    },
+    methods:{
+      addGoal(){
+        this.goals.push(this.enteredValue);
+        this.enteredValue ="" ;
+      },
+     removeGoal(index){
+      this.goals.splice(index,1);
+     }
+    }
+      
+
+      
+  });
+  
+  app.mount("#app");
